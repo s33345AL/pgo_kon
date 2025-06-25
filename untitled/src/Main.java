ba5b06880bb0dcd9cf45b70fb5a793ad26a2fd8b
Antoni <s33345@pjwstk.edu.pl>
2025-06-25 13:23:14 +0200

    koniec

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -7,7 +7,6 @@ public class Main {
    KontenerNaPlyny kon1 = new KontenerNaPlyny(20, 15, 45, 22, "2242", 25, true);
    KontenerGaz kon2 = new KontenerGaz(40, 21, 900, 22, "2156", 50);
    KontenerChlodniczy kon3 = new KontenerChlodniczy(25, 22, 150, 22, "2125", 60, "banany", 13.3, 20);
-Kontenerowiec statek2 = new Kontenerowiec(25, 10, 1000);
    kon1.zaladowanieKontener();
    kon2.zaladowanieKontener();
    kon3.zaladowanieKontener();
@@ -15,7 +14,6 @@ Kontenerowiec statek2 = new Kontenerowiec(25, 10, 1000);
    statek1.zaladowanieKonteneraNaStatek(kon1);
    statek1.zaladowanieKonteneraNaStatek(kon2);
    statek1.zaladowanieKonteneraNaStatek(kon3);
-
 
    statek1.wyswietlInformacjeOStatku();
     }catch (Exception e){
